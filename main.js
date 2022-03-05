b12563c70ceccc020175902f5f01f7cb510bad5f
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2022-03-05 18:33:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 //https://teachablemachine.withgoogle.com/models/895gkxgms/
+function StartClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/dIwCi5ahW/model.json',modelReady);
+}
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+function gotResults(error,results)
+{
+console.log('got results')
+}
