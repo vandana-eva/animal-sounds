0feb52a91af6c458834d74279b68fbe0401efa45
vandana-eva <92171611+vandana-eva@users.noreply.github.com>
2022-03-18 18:31:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-//https://teachablemachine.withgoogle.com/models/895gkxgms/
 function StartClassification()
 {
     navigator.mediaDevices.getUserMedia({audio:true});
-    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/dIwCi5ahW/model.json',modelReady);
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/895gkxgms/model.json',modelReady);
 }
 function modelReady()
 {
@@ -11,4 +10,26 @@ function modelReady()
 function gotResults(error,results)
 {
 console.log('got results')
+}
+function gotResults(error,results) {
+    if(error){
+        console.error(error);
+    } 
+    else
+    {
+        console.log(results);
+     random_number_r=Math.floor(Math.random()*255)+1;
+     random_number_b=Math.floor(Math.random()*255)+1;
+     random_number_g=Math.floor(Math.random()*255)+1;
+     
+     document.getElementById("result_label").innerHTML='i can hear- '+results[0].label;
+     document.getElementById("accuracy").innerHTML='accuracy-'+(results[0].confidence*100).toFixed(2)+"%";
+     document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+     document.getElementById("accuracy").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+     
+     img1=document.getElementById('alien_1');
+     img2=document.getElementById('alien_2');
+     img3=document.getElementById('alien_3');
+     img4=document.getElementById('alien_4');
+    }
 }
